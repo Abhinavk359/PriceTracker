eca78f3ed67ba55ea2067e4be294f746fe07a0ed
Abhinav kumar <kumarabhinav14122@gmail.com>
2024-07-17 18:47:06 +0530

    fixed build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,6 +4,9 @@ import { connectToDB } from "@/lib/mongoose";
 import { generateEmailBody, sendEmail } from "@/lib/nodeMailer";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
+export const maxDuration=300;
+export const dynamic='force-dynamic';
+export const revalidate=0;
 export async function GET() {
     try{
        connectToDB();
@@ -27,7 +30,7 @@ export async function GET() {
                 averagePrice:getAveragePrice(updatedPriceHistory),
              }
 
-           const updatedProduct=await Product.findOneAndUpdate({url:scrapedProduct.url},
+           const updatedProduct=await Product.findOneAndUpdate({url:product.url},
            product,
            );
 
